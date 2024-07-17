053419c05cd0bbd26577706bf8c92fb72d7b12db
abhilasha0623 <abhilasha.92.kp@gmail.com>
2024-07-17 19:23:37 +0530

    AddGoal

--- Vue-Firebase/app.js
+++ Vue-Firebase/app.js
@@ -1,4 +1,13 @@
-const app=Vue.creatApp()
+const buttonEl = document.querySelector('button');
+const inputEl = document.querySelector('input');
+const listEl = document.querySelector('ul');
 
+function addGoal() {
+  const enteredValue = inputEl.value;
+  const listItemEl = document.createElement('li');
+  listItemEl.textContent = enteredValue;
+  listEl.appendChild(listItemEl);
+  inputEl.value = '';
+}
 
-app.mount("#app")
+buttonEl.addEventListener('click', addGoal);
